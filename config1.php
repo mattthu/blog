2cfdbfd764fa8ebd8c218175b7a73fa1ca8b1160
mattthu <matthias.kurs@khk.ee>
2018-05-29 19:57:50 +0300

    konfiguratsiooni fail

---
+++ config1.php
@@ -0,0 +1,21 @@
+<?php
+// Local configuration
+define('DATABASE_HOSTNAME', 'localhost');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', 'root');
+define('DATABASE_DATABASE', 'blog');
+define('SMTP_AUTH_PASSWORD', '');
+define('SMTP_AUTH_USERNAME', '');
+define('SMTP_AUTH', false);
+define('SMTP_DEBUG', 0);
+define('SMTP_ENCRYPTION', 'none');
+define('SMTP_FROM', PROJECT_NAME . '@example.com');
+define('SMTP_HOST', 'localhost');
+define('SMTP_PORT', 1025); // 1025 - Mailhog, 25 - live
+define('SMTP_USE_SENDMAIL', false); // Send mail via system sendmail command or directly via SMTP
+define('SMTP_DKIM_ENABLED', false);
+define('SMTP_DKIM_DOMAIN', 'example.com');
+define('SMTP_DKIM_PRIVATE_KEY', '.certs/dkim_private_key.pem');
+define('SMTP_DKIM_SELECTOR', 'example');
+define('SMTP_DKIM_PASSPHRASE', '');
+define('SMTP_DKIM_IDENTITY', SMTP_FROM);
