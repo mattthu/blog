920e60867fa7a09c807740b8a4a827f76d687aba
mattthu <matthias.kurs@khk.ee>
2018-05-29 21:41:25 +0300

    kommentaarid

--- controllers/posts.php
+++ controllers/posts.php
@@ -13,6 +13,7 @@ class posts extends Controller
     {
 
         $this->posts = get_all("SELECT * FROM post");
+        $this->posts = get_all("SELECT * from post NATURAL JOIN users");
         $this->tags = get_all("SELECT tag_name,tag_id, COUNT(post_id) AS count FROM post_tags NATURAL JOIN tag GROUP BY tag_id");
     }
 
@@ -59,5 +60,11 @@ function view()
     $post_id = $this->params[0];
     $this->post = get_first("SELECT * FROM post NATURAL JOIN users WHERE post_id='$post_id'");
     $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
+    $this->comms = get_all("SELECT * FROM post_comments NATURAL JOIN comment WHERE post_id='$post_id'");
+    +    }
++    function ins()
++    {
+    +      $post_id = $this->params[0];
+    +      $this->post = get_first("SELECT * FROM post WHERE post_id='$post_id'");
 }
 }
